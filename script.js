46e85ba0a4b968f12d9eb900c1c3d7d938da3108
Shadi <89422699+shadiwin@users.noreply.github.com>
2022-03-04 13:21:30 +0000

    Level 400 and styles updated

--- script.js
+++ script.js
@@ -1,9 +1,22 @@
 //You can edit ALL of the code here
 let rootTag = document.getElementById("root");
 
-function setup() {
-  const allEpisodes = getAllEpisodes();
+const getAllEpisodesHandler = () => {
+  const all = getAllShows();
+  // const random = Math.floor(Math.random() * all.length - 1) + 1;
 
+  return fetch(`https://api.tvmaze.com/shows/82/episodes`)
+    .then(res => res.json())
+    .then(allEpisodes => {
+      return rendering(allEpisodes, all);
+    })
+    .catch(er => console.log(er));
+}
+
+
+function rendering(allEpisodes, all) {
+
+  let myEpisodes = allEpisodes;
   //creat a function for Season & Episode number, which is going to be used at level 100
   function creatTwoDigits(n) {
     return n.toString().padStart(2, '0');
@@ -11,7 +24,55 @@ function setup() {
   //level 100:=================================================================================================
   let articleTag = document.createElement("article");
   articleTag.className = "episodes_article";
-  //creating a function to show episodes
+  //Header:
+  let headerTag = document.createElement("header");
+  headerTag.className = "header_container";
+  rootTag.appendChild(headerTag);
+
+  let navTag = document.createElement("nav");
+  navTag.className = "nav_bar";
+  headerTag.appendChild(navTag);
+
+  let divMenuTag = document.createElement("div");
+  divMenuTag.className = "menu";
+  navTag.appendChild(divMenuTag);
+
+
+
+
+  let divBurger = document.createElement("div");
+  headerTag.appendChild(divBurger);
+  divBurger.className = "burger_menu";
+
+  const myMenu = ["Login", "News", "Shopping", "Contact"];
+
+  let divNavBurger = document.createElement("div");
+  divNavBurger.className = "burger_container";
+  let divTopBurger = document.createElement("div");
+  let divMiddleBurger = document.createElement("div");
+  let divBottomBurger = document.createElement("div");
+  divTopBurger.className = "top";
+  divMiddleBurger.className = "middle";
+  divBottomBurger.className = "bottom";
+  divNavBurger.append(divTopBurger, divMiddleBurger, divBottomBurger);
+
+  let ulBurgur = document.createElement("ul");
+  ulBurgur.setAttribute("class", "ul_burger");
+
+  navTag.appendChild(divNavBurger);
+  for (let i = 0; i < myMenu.length; i++) {
+    let liTag = document.createElement("li");
+    liTag.innerHTML = `<a href=#${myMenu[i]}>${myMenu[i]}</a>`
+    ulBurgur.appendChild(liTag);
+  }
+  divBurger.appendChild(ulBurgur.cloneNode(true));
+  divMenuTag.appendChild(ulBurgur.cloneNode(true));
+
+
+  //=============================================================creating a function to show episodes
+  let inputTag = document.createElement("input");
+  let selectTag = document.createElement("select");
+
   const episodesHandler = (myArray) => {
     //this while loop will reset our articleTag when articleTag.hasChildNodes is true, otherwise any input would be added to 73 episodes, otherwise we would get filtered episode plus all the 73 episodes
     while (articleTag.hasChildNodes()) {
@@ -30,7 +91,14 @@ function setup() {
       articleTag.appendChild(cardDiv);
 
       let imgTag = document.createElement("img");
-      imgTag.src = obj.image.medium;
+      //adding an if statement to make sure that we have replacement for those none-medium objects
+      // imgTag.src = obj.image.medium;
+      if (obj.image) {
+        imgTag.src = obj.image.medium;
+      } else {
+        imgTag.src = "https://cdn.dribbble.com/users/2226349/screenshots/12050930/media/1656dfed347dd8bcb9df5b15aa46908e.jpg?compress=1&resize=1200x900&vertical=top";
+      }
+
       cardDiv.appendChild(imgTag);
 
       let h1Tag = document.createElement("h1");
@@ -43,20 +111,63 @@ function setup() {
       cardDiv.appendChild(pTag);
 
     })
+
+
   }
-  episodesHandler(allEpisodes)
+  episodesHandler(allEpisodes);
+
+  // Search input listener and filtering depending on movie
+  inputTag.addEventListener("input", (event) => {
+
+    let myDisplay = myEpisodes.filter(obj =>
+      obj.name.includes(event.target.value) || obj.summary.includes(event.target.value)
+    );
+    spanTag.innerText = `Displaying ${myDisplay.length}/${allEpisodes.length} episodes`;
+    //call our new array throght episodeHandler function
+    episodesHandler(myDisplay);
+  })
+  //=================================================================Listener for select Episodes tag
+  selectTag.addEventListener("change", () => {
+    const myArray = myEpisodes.filter(obj => obj.name === selectTag.value);
+    myArray.length === 0 ? episodesHandler(myEpisodes) : episodesHandler(myArray);
+
+  })
+
+  //======================================= Select tag listener and filtering depending on movie
+  const createSelectEpisodes = (arr) => {
+
+    selectTag.innerHTML = "";
+    arr.forEach(obj => {
+
+      let optionTag = document.createElement("option");
+      optionTag.setAttribute("value", obj.name);
+      optionTag.innerHTML = `S${creatTwoDigits(obj.season)}E${creatTwoDigits(obj.number)} - ${obj.name}`;
+      selectTag.appendChild(optionTag);
+
+
+    })
+    let noneValueOption = document.createElement("option");
+    noneValueOption.value = "none";
+    noneValueOption.innerText = "Not Selected";
+    selectTag.appendChild(noneValueOption);
+    noneValueOption.selected = true;
+
+  }
+  createSelectEpisodes(myEpisodes);
 
   //to create Search Bar. level 200:========================================================================
   let divSearchBar = document.createElement("div");
-  rootTag.appendChild(divSearchBar);
+  navTag.appendChild(divSearchBar);
+
+
 
   let spanTag = document.createElement("span");
   spanTag.className = "input_span";
-  divSearchBar.appendChild(spanTag);
+  spanTag.innerText = `Displaying ${myEpisodes.length}/${allEpisodes.length} episodes`;
+
 
   divSearchBar.setAttribute("id", "searchWarpper");
-  rootTag.appendChild(divSearchBar);
-  let inputTag = document.createElement("input");
+  navTag.appendChild(divSearchBar);
   inputTag.setAttribute("type", "text");
   inputTag.setAttribute("name", "searchBar");
   inputTag.setAttribute("id", "searchBar");
@@ -66,42 +177,49 @@ function setup() {
   rootTag.appendChild(articleTag);
 
   //add event to input for to show live search
-  inputTag.addEventListener("input", (event) => {
 
-    let myDisplay = allEpisodes.filter(obj =>
-      obj.name.includes(event.target.value) || obj.summary.includes(event.target.value)
-    );
-    spanTag.innerText = `Displaying ${myDisplay.length}/${allEpisodes.length} episodes`;
-    //call our new array throght episodeHandler function
-    episodesHandler(myDisplay);
-  })
   //level 300:===============================================================================================
-  let selectTag = document.createElement("select");
+
   divSearchBar.appendChild(selectTag);
   selectTag.setAttribute("class", "episode");
 
-  allEpisodes.forEach(obj => {
-    let optionTag = document.createElement("option");
-    optionTag.setAttribute("value", obj.name);
-    optionTag.innerHTML = `S${creatTwoDigits(obj.season)}E${creatTwoDigits(obj.number)} - ${obj.name}`;
-    selectTag.appendChild(optionTag);
+
+
+
+  //level 400:=====================================================
+  let selectFavTag = document.createElement("select");
+  selectFavTag.setAttribute("id", "fav_tvshow");
+  divSearchBar.appendChild(selectFavTag);
+
+
+
+  const sortedArray = all.sort((a, b) => a.name.localeCompare(b.name));
+  all.forEach(obj => {
+
+    let optionFavTag = document.createElement("option");
+    optionFavTag.innerHTML = obj.name;
+    optionFavTag.value = `${obj.id}`;
+    selectFavTag.appendChild(optionFavTag);
+
 
   })
-  let noneValueOption = document.createElement("option");
-  noneValueOption.value = "none";
-  noneValueOption.innerText = "Not Selected";
-  selectTag.appendChild(noneValueOption);
-  noneValueOption.selected = true;
 
-  //add change event 
-  selectTag.addEventListener("change", () => {
-    const filteredEpisodes = allEpisodes.filter(obj =>
-      obj.name === selectTag.value
-    )
-    //if value of selectTag is not selected, allEpisodes should be shown at the beginning
-    selectTag.value === "none" ? episodesHandler(allEpisodes) : episodesHandler(filteredEpisodes);
+
+  selectFavTag.addEventListener("change", (e) => {
+    fetch(`https://api.tvmaze.com/shows/${e.target.value}/episodes`)
+      .then(res => {
+        if (res && res.ok) {
+          return res.json()
+        }
+      })
+      .then(data => {
+        myEpisodes = data;
+        createSelectEpisodes(data);
+        return episodesHandler(data)
+      })
   })
+
 }
 
 
-window.onload = setup;
+window.onload = getAllEpisodesHandler;
